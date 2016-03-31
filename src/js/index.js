4df8be0f60cd9e813b4e045927798fc6ee1a8c62
JONATHAN SILVA <silva.jon86@gmail.com>
2016-03-31 07:02:12 -0400

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// Javascript Entry Point
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import User from './user';
+
+
+ReactDOM.render (
+<App/>, 
+document.querySelector('.app')
+);
